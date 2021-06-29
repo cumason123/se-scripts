68e362140dcd8287bc8f8c45d4fba4d283f086ea
Curtis Mason <cumason@bu.edu>
2021-06-29 16:05:39 -0700

    hostile detection

---
+++ hostile_detection.cs
@@ -0,0 +1,8 @@
+public void Main(string argument, UpdateType updateSource)
+{
+    IMySensorBlock panel = (IMySensorBlock)GridTerminalSystem.GetBlockWithName("Detection.Sensor");
+    MyDetectedEntityInfo entity = sensor.LastDetectedEntity();
+    if (entity.Relationship === MyRelationsBetweenPlayerAndBlock.Enemies) {
+        
+    }
+}
